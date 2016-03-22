047c75a033762fce8e93ad819f4d879ae56e8287
Mika Attila <radiantstatue@gmail.com>
2016-03-22 21:19:09 +0100

    Implement toml parsing of config file

--- src/config.rs
+++ src/config.rs
@@ -1,15 +1,44 @@
 use std::fs::File;
-use std::error::Error;
+use std::io;
 use std::io::prelude::*;
+use toml;
 
 pub const FILENAME: &'static str = "noten.toml";
 
 pub struct Config;
 
-pub fn read() -> Result<Config, Box<Error>> {
+quick_error! {
+    #[derive(Debug)]
+    pub enum ReadError {
+        Io(err: io::Error) {
+            from()
+        }
+        TomlParser(msg: String) {}
+    }
+}
+
+pub fn read() -> Result<Config, ReadError> {
     let mut file = try!(File::open(FILENAME));
     let mut text = String::new();
     try!(file.read_to_string(&mut text));
-    println!("Config: {}", text);
+    let mut parser = toml::Parser::new(&text);
+    let table = match parser.parse() {
+        Some(table) => table,
+        None => {
+            let mut msg = String::new();
+            for e in &parser.errors {
+                let (lo_line, lo_col) = parser.to_linecol(e.lo);
+                let (hi_line, hi_col) = parser.to_linecol(e.hi);
+                msg.push_str(&format!("{}:{} -> {}:{} : {}",
+                                      lo_line + 1,
+                                      lo_col + 1,
+                                      hi_line + 1,
+                                      hi_col + 1,
+                                      e.desc));
+            }
+            return Err(ReadError::TomlParser(msg));
+        }
+    };
+    println!("{:#?}", table);
     Ok(Config)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
+extern crate toml;
+#[macro_use]
+extern crate quick_error;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
 mod config;
 
+use config::ReadError;
+
 fn main() {
+    env_logger::init().unwrap();
+
     match config::read() {
         Ok(_) => println!("Okay"),
-        Err(e) => {
-            println!("Failed opening {} ({}). Not a valid noten project.",
-                     config::FILENAME,
-                     e)
+        Err(ReadError::Io(err)) => {
+            error!("Failed opening {} ({}). Not a valid noten project.",
+                   config::FILENAME,
+                   err)
         }
+        Err(ReadError::TomlParser(msg)) => error!("Failed to parse {}:\n{}", config::FILENAME, msg),
     }
 }
