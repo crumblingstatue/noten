612e0489cebdab7e71882d36afd73c0a29ff5051
Mika Attila <radiantstatue@gmail.com>
2016-03-23 22:56:36 +0100

    Have nicely formatted cargo/rustc output for generators

--- src/substitution.rs
+++ src/substitution.rs
@@ -2,6 +2,8 @@ use std::error::Error;
 use config::Config;
 use regex::{Captures, Regex};
 use std::path::Path;
+use std::io;
+use std::io::prelude::*;
 
 fn expand_constants(command: &str, config: &Config) -> String {
     let re = Regex::new("%([a-z-]+)").unwrap();
@@ -43,9 +45,18 @@ fn gen(gen_name: &str, args: &[&str], generators_dir: &Path) -> Result<String, B
         panic!("{:?} does not exist.", generator_dir);
     }
     let mut cmd = Command::new("cargo");
-    cmd.current_dir(&generator_dir).arg("build").arg("--release");
+    cmd.current_dir(&generator_dir)
+       .arg("rustc")
+       .arg("--release")
+       .arg("--color=always")
+       .arg("--")
+       .arg("--color=always");
     let output = cmd.output().expect("Failed to spawn cargo");
-    println!("{:?}", output);
+    let _ = io::stdout().write_all(&output.stdout);
+    let _ = io::stderr().write_all(&output.stderr);
+    if !output.status.success() {
+        panic!("cargo failed");
+    }
     let gen_cmd_path = generator_dir.join(format!("target/release/{}", gen_name));
     debug!("Gen command path is {:?}", gen_cmd_path);
     let mut gen_cmd = Command::new(&gen_cmd_path);
